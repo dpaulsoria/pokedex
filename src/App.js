867e0cb46ac85764a5da7fced866b5d43fc56ea1
Paul Soria <dpaulsoria@gmail.com>
2024-01-15 06:58:31 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Test from "./components/test";
 
 export default function App() {
   return (
     <>
       <h1>Hola mundo!</h1>
+      <Test />
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import './index.css'; // Asegúrate de que la ruta coincida con tu archivo CSS
+
+
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(<App tab="home" />);
